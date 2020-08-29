3a0dffc08f571f4ce4e4b93de6e0002b36ecba67
cal sandaruwan <lsblackcyber@gmail.com>
2020-08-29 15:45:15 +0530

    Tailwingcss configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './assets/main.css';
 import {TodoContainer} from "./components/todo/todoContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <TodoContainer/>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
     </div>
   );
 }
